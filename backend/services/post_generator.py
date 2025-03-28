44cc6a9a9e0b64127f5efca96f21253a56b7134d
Andrew Harris <Andrew.Harris@torontomu.ca>
2025-03-28 16:24:10 -0400

    fixed author tag

--- backend/services/post_generator.py
+++ backend/services/post_generator.py
@@ -1,11 +1,15 @@
 # post_generator.py
 
+from backend.services.post_to_linkedin import LINKEDIN_USER_ID
+
+
 def generate_post_from_webhook(payload):
     repo = payload.get("repository", {}).get("name", "a GitHub repo")
     url = payload.get("repository", {}).get("html_url", "https://github.com")
     commit = payload.get("head_commit", {})
     message = commit.get("message", "made an update")
-    author = commit.get("author", {}).get("name", "Someone")
+    author = f"urn:li:member:{LINKEDIN_USER_ID}"
+
 
     return (
         f"🚀 {author} just pushed to {repo}!\n\n"
